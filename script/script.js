b5dbec04b909baef1be9f12f5363d02333ca8e3f
DilmurodjonT <dilmurod_tadjibaev@mail.ru>
2022-12-09 23:27:42 +0500

    chench

--- script/script.js
+++ script/script.js
@@ -19,15 +19,3 @@ form.addEventListener("submit", function (event) {
   }
   // console.log(inputNumber.value);
 });
-
-// for (i = 1; i < 100; i++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log("FizzBuzz");
-//   } else if (i % 3 === 0) {
-//     console.log("Fizz");
-//   } else if (i % 5 === 0) {
-//     console.log("Buzz");
-//   } else {
-//     console.log(i);
-//   }
-// }
